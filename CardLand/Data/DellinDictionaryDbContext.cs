9183bdcd1a1099a1e10f8d685e6a98214768f608
HenryGr <henry_graham@mail.ru>
2026-03-08 14:05:37 +0700

    Added: DbContext

---
+++ CardLand/Data/DellinDictionaryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CardLand.Data
+{
+    public class DellinDictionaryDbContext : DbContext
+    {
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
