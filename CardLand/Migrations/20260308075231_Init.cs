48171de32bf623794f3ba638a95ace2ab0309433
HenryGr <henry_graham@mail.ru>
2026-03-08 15:03:24 +0700

    Added: Init migration

---
+++ CardLand/Migrations/20260308075231_Init.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CardLand.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "text", nullable: true),
+                    CityCode = table.Column<int>(type: "integer", nullable: false),
+                    Uuid = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<string>(type: "text", nullable: true),
+                    CountryCode = table.Column<string>(type: "text", nullable: false),
+                    CoordinatesLatitude = table.Column<double>(type: "double precision", nullable: false),
+                    CoordinatesLongitude = table.Column<double>(type: "double precision", nullable: false),
+                    AddressRegion = table.Column<string>(type: "text", nullable: true),
+                    AddressCity = table.Column<string>(type: "text", nullable: true),
+                    AddressStreet = table.Column<string>(type: "text", nullable: true),
+                    AddressHouseNumber = table.Column<string>(type: "text", nullable: true),
+                    AddressApartment = table.Column<int>(type: "integer", nullable: true),
+                    WorkTime = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OfficePhones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OfficeId = table.Column<int>(type: "integer", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Additional = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfficePhones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OfficePhones_Offices_OfficeId",
+                        column: x => x.OfficeId,
+                        principalTable: "Offices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OfficePhones_OfficeId",
+                table: "OfficePhones",
+                column: "OfficeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OfficePhones");
+
+            migrationBuilder.DropTable(
+                name: "Offices");
+        }
+    }
+}
